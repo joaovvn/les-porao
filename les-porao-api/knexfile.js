30da47369f5db7a6fd14e4585998e7d211736850
João Marcos Cerqueira <63267158+joaomarcosbc@users.noreply.github.com>
2022-04-27 20:41:54 -0300

    fix: connect database

--- les-porao-api/knexfile.js
+++ les-porao-api/knexfile.js
@@ -3,11 +3,12 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DATABASE_HOST,
+      host: 'ec2-3-224-125-117.compute-1.amazonaws.com',
       port: 5432,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE
+      user: 'dcqclqatfvweca',
+      password: 'd11ca0d9adec21d8cd2f8640b45a4df9f1aeac20699eb88bc27f930e8b1d787d',
+      database: 'd2f1mfb6ftaf8n',
+      ssl: {rejectUnauthorized: false}
     },
     migrations: {
       tableName: "migrations",
