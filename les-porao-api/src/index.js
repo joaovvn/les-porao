f38cba15207b007cc4faf35212d65ad781c40e55
João Marcos Cerqueira <63267158+joaomarcosbc@users.noreply.github.com>
2022-04-27 16:44:20 -0300

    teste

--- les-porao-api/src/index.js
+++ les-porao-api/src/index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.get('/teste', (req, res) => {
+   res.send('API OK ERRO 200')
+})
+
 app.listen(8000, () => {
    console.log("Servidor rodando na porta 8000!")
 });
