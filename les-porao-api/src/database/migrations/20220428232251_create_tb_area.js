e2abae84054d48a0a321540d12f967ff55d3576c
João Marcos <joaombcerqueira@gmail.com>
2022-04-30 16:19:58 -0300

    fix: droptable cargo

--- les-porao-api/src/database/migrations/20220428232251_create_tb_area.js
+++ les-porao-api/src/database/migrations/20220428232251_create_tb_area.js
@@ -2,22 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable('area', (table) => {
-    table.increments('id').primary()
-    table.text('name')
-    table.integer('status').defaultTo(1)
+exports.up = function (knex) {
+  return knex.schema.createTable("area", (table) => {
+    table.increments("id").primary();
+    table.text("name");
+    table.integer("status").defaultTo(1);
 
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
-
-  })
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTable('area');
+exports.down = function (knex) {
+  return knex.schema.dropTable("area");
 };
