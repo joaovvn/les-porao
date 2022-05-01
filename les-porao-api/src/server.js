f4eaac8a9c8a9dd91488047d97c62fbea6451836
ferreirawalter <wferreiraramosjunior@gmail.com>
2022-05-01 10:20:47 -0300

    fix: cors enabled

--- les-porao-api/src/server.js
+++ les-porao-api/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes/index.routes');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 8000
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.get('/', (req, res) => {
    res.send('API OK ERRO 200')
